303fd579b658f989ebcf1188a0ce54bf1b89cc4a
Miskat-BD <mishkatahmed321@gmail.comgit config --global user.name Miskat-BDgit commit -m first>
2026-05-18 16:37:15 +0600

    fetch all tutors in frontend

--- src/app/tutors/page.jsx
+++ src/app/tutors/page.jsx
@@ -1,8 +1,16 @@
 
-const TutorsPage = () => {
+const TutorsPage = async () => {
+    const res = await fetch('http://localhost:8080/tutors');
+    const tutors = await res.json();
+    console.log(tutors);
     return (
         <div>
-            all tutors
+            <h1 className="text-2xl font-bold">All Tutors</h1>
+            <div className="">
+            {
+                tutors.map(tutor => <div key={tutor._id}>{tutor.name}</div>)
+            }
+            </div>
         </div>
     );
 };
